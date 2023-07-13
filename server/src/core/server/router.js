dd99f37abe387a1aeace9d3770c239184d81337d
Romanelli <romanelli@programist.ru>
2023-07-12 20:29:31 -0400

    feat: add rootPattern resolving logic in router

--- server/src/core/server/router.js
+++ server/src/core/server/router.js
@@ -1,5 +1,8 @@
 ///Message ROUTING///
 
+//TODO: replace with resolve()
+const { isRegExp: isRoutePatternDynamic } = require("../../utils/regexpHelper");
+
 // [ ] 1. add middleware logic (for: log, parse, error, rate limit) - it's close to routing but could have next() inside the body to 'pass the control' to the next handler;
 // [ ]   1.0. also, multiple callbacks on one pattern
 // [ ]   1.1. also, add the "next" method to props and call it to move to the next matching handler
@@ -7,9 +10,6 @@
 // [ ] 2. add "error handling" using routing mechanism (call bindRoute(*) after all other bindings in each state) ?
 
 module.exports = (staticRouter, dynamicRouter, rootPattern) => {
-  //TODO: add logic to "bind" and "unbind" functions to get full path before working with "newRouter": "/" + "path" + "route".
-  //      "RegEx" + "string" ref: https://masteringjs.io/tutorials/fundamentals/concat-regexp
-
   //init
   const {
     doStaticRouteExist,
@@ -29,7 +29,38 @@ module.exports = (staticRouter, dynamicRouter, rootPattern) => {
   } = dynamicRouter;
 
   //private functions
-  const isRoutePatternDynamic = (pattern) => pattern instanceof RegExp;
+  const formatRoutePath = (path) =>
+    typeof path === "string" && !path.startsWith("/") ? `/${path}` : path;
+
+  const getSubPathIndex = (path) => {
+    let matchLength = 0;
+    if (isRoutePatternDynamic(rootPattern)) {
+      const matches = rootPattern.exec(path);
+      if (matches) {
+        matchLength = matches[0].length;
+      }
+    } else {
+      if (path.startsWith(rootPattern)) {
+        matchLength = rootPattern.length;
+      }
+    }
+    return matchLength;
+  };
+
+  const tryInvokeRoute = (route, data, remote) => {
+    const canInvokeRoute = route?.isEnabled && route.handler;
+    canInvokeRoute && route.handler(data, remote);
+    return !!canInvokeRoute;
+  };
+
+  const tryInvokeSubRoute = (path, data, remote) => {
+    const staticRoute = getStaticRoute(path);
+    if (staticRoute) return tryInvokeRoute(staticRoute, data, remote);
+
+    //if path does not match any "static" pattern, try "dynamic" patterns
+    const dynamicRoute = getDynamicRouteByPath(path);
+    return tryInvokeRoute(dynamicRoute, data, remote);
+  };
 
   const tryToggleRoute = (pattern, doEnable) => {
     const route = isRoutePatternDynamic(pattern)
@@ -38,12 +69,6 @@ module.exports = (staticRouter, dynamicRouter, rootPattern) => {
 
     route && (route.isEnabled = doEnable);
     return !!route;
-  };
-
-  const tryInvokeRoute = (route, data, remote) => {
-    const canInvokeRoute = route?.isEnabled && route.handler;
-    canInvokeRoute && route.handler(data, remote);
-    return !!canInvokeRoute;
   };
 
   //public functions
@@ -76,13 +101,20 @@ module.exports = (staticRouter, dynamicRouter, rootPattern) => {
     !!getStaticRoute(pattern)?.isEnabled ||
     !!getDynamicRoute(pattern)?.isEnabled;
 
-  const tryInvoke = (path, data, remote) => {
-    const staticRoute = getStaticRoute(path);
-    if (staticRoute) return tryInvokeRoute(staticRoute, data, remote);
+  const tryInvoke = (data, remote) => {
+    if (!data?.action) return false;
 
-    //if path does not match any "static" pattern, try "dynamic" patterns
-    const dynamicRoute = getDynamicRouteByPath(path);
-    return tryInvokeRoute(dynamicRoute, data, remote);
+    data.action = formatRoutePath(data.action);
+    const pathStartingIndex = getSubPathIndex(data.action);
+
+    return (
+      pathStartingIndex > 0 &&
+      tryInvokeSubRoute(
+        formatRoutePath(data.action.slice(pathStartingIndex)),
+        data,
+        remote
+      )
+    );
   };
 
   return {

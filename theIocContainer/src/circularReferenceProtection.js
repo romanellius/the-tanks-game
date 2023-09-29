b77a1ed12f938e3cc88b63eecff37abc13e1986b
Romanelli <romanelli@programist.ru>
2023-09-28 22:05:45 -0400

    refactor: minor 'theIocContainer' changes

--- theIocContainer/src/circularReferenceProtection.js
+++ theIocContainer/src/circularReferenceProtection.js
@@ -1,6 +1,8 @@
 module.exports = () => {
+  //init
   const dependencies = new Set();
 
+  //private functions
   const add = (value) => {
     if (dependencies.has(value)) {
       throw `IoC Container: Can not resolve '${value}' dependency: circular referencing detected`;
@@ -12,8 +14,16 @@ module.exports = () => {
     dependencies.delete(value);
   };
 
+  //public functions
+  const protect = (handler, value, props) => {
+    add(value);
+    const result = handler(value, props);
+
+    remove(value);
+    return result;
+  };
+
   return {
-    addDependencyToCircularProtection: add,
-    removeDependencyFromCircularProtection: remove,
+    protectFromCircularReferencing: protect,
   };
 };
--- theIocContainer/src/index.js
+++ theIocContainer/src/index.js
@@ -2,95 +2,81 @@ const isPlainObject = require("lodash.isplainobject");
 const forEach = require("lodash.foreach");
 const useCircularProtection = require("./circularReferenceProtection");
 
-//private functions
-const validateConfiguration = (configObject) => {
-  if (!isPlainObject(configObject)) {
-    throw "IoC Container: 'dependencies.js' must export an object";
-  }
-
-  forEach(configObject, (dependency) => {
-    if (!isPlainObject(dependency)) {
-      throw "IoC Container: each dependency must be an object in 'dependencies.js'";
-    }
-
-    if (!dependency.handler) {
-      throw "IoC Container: each dependency must have a handler in 'dependencies.js'";
-    }
-
-    const { dependencies: relatedDependencies } = dependency;
-    if (relatedDependencies) {
-      if (!(relatedDependencies instanceof Array)) {
-        throw "IoC Container: related dependencies must be boxed in an array in 'dependencies.js'";
-      }
-
-      relatedDependencies.forEach((relatedDependency) => {
-        if (typeof relatedDependency !== "string") {
-          throw "IoC Container: each related dependency must be a string in 'dependencies.js'";
-        }
-
-        if (!configObject[relatedDependency]) {
-          throw "IoC Container: each related dependency must be presented in 'dependencies.js'";
-        }
-      });
-    }
-  });
-};
-
-const registerDependency = (
-  container,
-  name,
-  argDependencies = [],
-  handler,
-  isSingleton
-) => {
-  if (container[name]) {
-    throw `IoC Container: Can not register '${name}' dependency: occupied`;
-  }
-
-  container[name] = { argDependencies, handler, isSingleton };
-};
-
-const bindConfiguration = (container, configObject) => {
-  forEach(configObject, (dependency, dependencyName) => {
-    const { handler, dependencies, isSingleton } = dependency;
-
-    registerDependency(
-      container,
-      dependencyName,
-      dependencies ?? [],
-      handler,
-      !!isSingleton
-    );
-  });
-};
-
-const build = (container, configObject) => {
-  validateConfiguration(configObject);
-  bindConfiguration(container, configObject);
-};
-
 module.exports = (configObject) => {
+  //init
+  const { protectFromCircularReferencing } = useCircularProtection();
+
   const container = {};
   build(container, configObject);
 
-  const {
-    addDependencyToCircularProtection,
-    removeDependencyFromCircularProtection,
-  } = useCircularProtection();
+  //private functions
+  function validateConfiguration(configObject) {
+    if (!isPlainObject(configObject)) {
+      throw "IoC Container: 'dependencies.js' must export an object";
+    }
 
-  //public functions
-  const register = function (name, handler, argDependencies = []) {
-    registerDependency(container, name, argDependencies, handler, false);
-    return this;
-  };
-  const registerSingleton = function (name, handler, argDependencies = []) {
-    registerDependency(container, name, argDependencies, handler, true);
-    return this;
-  };
+    forEach(configObject, (dependency) => {
+      if (!isPlainObject(dependency)) {
+        throw "IoC Container: each dependency must be an object in 'dependencies.js'";
+      }
 
-  const resolve = (name, ...props) => {
-    addDependencyToCircularProtection(name);
+      if (!dependency.handler) {
+        throw "IoC Container: each dependency must have a handler in 'dependencies.js'";
+      }
 
+      const { dependencies: relatedDependencies } = dependency;
+      if (relatedDependencies) {
+        if (!(relatedDependencies instanceof Array)) {
+          throw "IoC Container: related dependencies must be boxed in an array in 'dependencies.js'";
+        }
+
+        relatedDependencies.forEach((relatedDependency) => {
+          if (typeof relatedDependency !== "string") {
+            throw "IoC Container: each related dependency must be a string in 'dependencies.js'";
+          }
+
+          if (!configObject[relatedDependency]) {
+            throw "IoC Container: each related dependency must be presented in 'dependencies.js'";
+          }
+        });
+      }
+    });
+  }
+
+  function registerDependency(
+    container,
+    name,
+    argDependencies = [],
+    handler,
+    isSingleton
+  ) {
+    if (container[name]) {
+      throw `IoC Container: Can not register '${name}' dependency: occupied`;
+    }
+
+    container[name] = { argDependencies, handler, isSingleton };
+  }
+
+  function bindConfiguration(container, configObject) {
+    forEach(configObject, (dependency, dependencyName) => {
+      const { handler, dependencies, isSingleton } = dependency;
+
+      registerDependency(
+        container,
+        dependencyName,
+        dependencies ?? [],
+        handler,
+        !!isSingleton
+      );
+    });
+  }
+
+  function build(container, configObject) {
+    validateConfiguration(configObject);
+    bindConfiguration(container, configObject);
+  }
+
+  function resolveDependency(name, props) {
     const dependency = container[name];
     if (!dependency) {
       throw `IoC Container: Dependency '${name}' not found`;
@@ -98,12 +84,10 @@ module.exports = (configObject) => {
 
     const { isSingleton, handler, argDependencies, instance } = dependency;
     if (typeof handler !== "function") {
-      removeDependencyFromCircularProtection(name);
       return handler;
     }
 
     if (isSingleton && instance) {
-      removeDependencyFromCircularProtection(name);
       return instance;
     }
 
@@ -115,13 +99,27 @@ module.exports = (configObject) => {
       props,
       resolve,
     });
+
     if (isSingleton) {
       dependency.instance = newInstance;
     }
 
-    removeDependencyFromCircularProtection(name);
     return newInstance;
-  };
+  }
+
+  //public functions
+  function register(name, handler, argDependencies = []) {
+    registerDependency(container, name, argDependencies, handler, false);
+    return this;
+  }
+  function registerSingleton(name, handler, argDependencies = []) {
+    registerDependency(container, name, argDependencies, handler, true);
+    return this;
+  }
+
+  function resolve(name, ...props) {
+    return protectFromCircularReferencing(resolveDependency, name, props);
+  }
 
   return {
     register,
